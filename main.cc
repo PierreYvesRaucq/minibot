4644d78538138743dede6500c4cb9db675352b51
pierre Heyvaert <56588950+pierreheyv@users.noreply.github.com>
2019-12-06 11:08:15 +0100

    Update main.cc

--- main.cc
+++ main.cc
@@ -18,20 +18,27 @@ int main()
     	printf("\t\t Please do not interchange the chips on my tower/motor PWM boards !\n");
     	printf("\t\t Try to respect the C-file interface when programming me because\n \t\t it will be the same in the robotic project (Q2) !\n");
 	
-	quit = 0;
-	CAN *can;
-	can = new CAN(CAN_BR);
+	quit = 0; // variable de sortie d'urgence
+	
+	// initialisation du CAN, de la structure et du speed controller
+	CAN *can; 
+	can = new CAN(CAN_BR); 
 	can->configure();
 	can->ctrl_motor(1);
 	CtrlStruct* structure = initalize_struct();
 	init_speed_controller(structure);
 	
+	//tourelle tourne à une valeur constante (valeur mise au hasard) 
+	can->push_TowDC(10);
+	
 	while(1){
-		angle;
-		rotation(angle);
-		avancement(D);
-		double cmdL = structure->theCtrlOut->wheel_commandsL;
-		double cmdR = structure->theCtrlOut->wheel_commandsL;
+		angle; // Bruno : fonction qui renvoie (ou actualise un paramêtre de la structure on verra après) les 2 angles de détection de l'obstacle
+		rotation(angle); //Pierre-Yves : fonction qui renvoie (ou actualise un paramêtre de la structure on verra après) les 2 commande à donner au roues pour que le robot tourne d'un angle theta
+		avancement(D); // Pierre : fonction qui renvoie (ou actualise un paramêtre de la structure on verra après) les 2 commande à donner au roues pour que le robot avance d'une dist D 
+		
+		//on va rechrcher le paramètre commande des roue dans la structure et on les applique
+		double cmdL = structure->theCtrlOut->wheel_commandsL; 
+		double cmdR = structure->theCtrlOut->wheel_commandsR;
 		printf("command0 %f, command1 %f\n", cmdL, cmdR);
 		can->push_PropDC(cmdL, cmdR);
 			
@@ -42,7 +49,3 @@ int main()
 	
 can->ctrl_motor(0);
 }
-
-
-}
-
