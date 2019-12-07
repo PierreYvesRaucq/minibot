6b48658d87fe399a135ac4d3237221ffe5278b86
pierre Heyvaert <56588950+pierreheyv@users.noreply.github.com>
2019-12-07 15:59:03 +0100

    Update main.cc

--- main.cc
+++ main.cc
@@ -4,6 +4,9 @@
 #include "IO/COM/CAN/CAN.hh"
 #include "IO/COM/SPI/Specific/SPI_CAN.hh"
 #include "IO/COM/SPI/SPI.hh"
+#include "./CONTROLLER/speed_controller.h"
+#include "./CONTROLLER/CtrlStruct.h"
+#include "./CONTROLLER/move.h"
 
 
 #define CAN_BR 125e3
@@ -17,35 +20,35 @@ int main()
     	printf("\t\t I'm Miss Sunshine, please take care of me !\n");
     	printf("\t\t Please do not interchange the chips on my tower/motor PWM boards !\n");
     	printf("\t\t Try to respect the C-file interface when programming me because\n \t\t it will be the same in the robotic project (Q2) !\n");
-	
-	quit = 0; // variable de sortie d'urgence
-	
+
+	int quit = 0; // variable de sortie d'urgence
+
 	// initialisation du CAN, de la structure et du speed controller
-	CAN *can; 
-	can = new CAN(CAN_BR); 
+	CAN *can;
+	can = new CAN(CAN_BR);
 	can->configure();
 	can->ctrl_motor(1);
 	CtrlStruct* structure = initalize_struct();
 	init_speed_controller(structure);
-	
-	//tourelle tourne à une valeur constante (valeur mise au hasard) 
+
+	//tourelle tourne à une valeur constante (valeur mise au hasard)
 	can->push_TowDC(10);
-	
+
 	while(1){
 		angle; // Bruno : fonction qui renvoie (ou actualise un paramêtre de la structure on verra après) les 2 angles de détection de l'obstacle
 		rotation(angle); //Pierre-Yves : fonction qui renvoie (ou actualise un paramêtre de la structure on verra après) les 2 commande à donner au roues pour que le robot tourne d'un angle theta
-		avancement(D); // Pierre : fonction qui renvoie (ou actualise un paramêtre de la structure on verra après) les 2 commande à donner au roues pour que le robot avance d'une dist D 
-		
+		fwd(D, goal, structure); // Pierre : fonction qui renvoie (ou actualise un paramêtre de la structure on verra après) les 2 commande à donner au roues pour que le robot avance d'une dist D
+
 		//on va rechrcher le paramètre commande des roue dans la structure et on les applique
-		double cmdL = structure->theCtrlOut->wheel_commandsL; 
-		double cmdR = structure->theCtrlOut->wheel_commandsR;
+		double cmdL = structure->theCtrlOut->wheel_commands[0];
+		double cmdR = structure->theCtrlOut->wheel_commands[1];
 		printf("command0 %f, command1 %f\n", cmdL, cmdR);
 		can->push_PropDC(cmdL, cmdR);
-			
+
 		if(quit ==1){
 			break;
 		}
 	}
-	
+
 can->ctrl_motor(0);
 }
