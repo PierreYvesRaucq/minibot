19deefa473a5f942edf9666ce975f0f0a79ebd5f
PierreYvesRaucq <58430139+PierreYvesRaucq@users.noreply.github.com>
2019-12-11 15:02:49 +0100

    Update Rotate.c

--- Rotate.c
+++ Rotate.c
@@ -6,11 +6,8 @@ void rotation(double theta, CtrlStruct* struct)
       double r = 0.03;
       double d = distance_entre_roues;
       
-      double angle_gauche = struct->theCtrlIn->angle_gauche;
-      double angle_droit = struct->theCtrlIn->angle_droit;
+      double comm_gauche = - 14*(d/2r)*theta;
+      double comm_droite = + 14*(d/2r)*theta;
       
-      double angle_comm_gauche = angle_gauche - 14*(d/2r)*theta;
-      double angle_comm_droite = angle_droite + 14*(d/2r)*theta;
-      
-      run_angle_controller(struct, angle_comm_gauche, angle_comm_droite);
+      run_angle_controller(struct, comm_gauche, comm_droite);
 }
