3078cf17bfaabda41aa841bf2e27c7cff2d6b7ee
PierreYvesRaucq <58430139+PierreYvesRaucq@users.noreply.github.com>
2019-12-11 16:12:10 +0100

    Update Rotate.c

--- Rotate.c
+++ Rotate.c
@@ -1,7 +1,7 @@
 #include "rotate.h"
 #include "angle_controller.h"
 
-void rotation(double theta, CtrlStruct* struct)
+void rotation(double theta, CtrlStruct* str)
 {      
       double r = 0.03;
       double d = 0.215;
@@ -9,17 +9,17 @@ void rotation(double theta, CtrlStruct* struct)
       double err_angle_gauche = -(d/(2*r))*theta;
       double err_angle_droite = (d/(2*r))*theta;
       
-      struct->theUserStruct->err_langle = err_angle_gauche;
-      struct->theUserStruct->err_rangle = err_angle_droite;
+      str->theUserStruct->err_langle = err_angle_gauche;
+      str->theUserStruct->err_rangle = err_angle_droite;
       
-      struct->theUserStruct->integ_err_l_prev = 0;
-      struct->theUserStruct->integ_err_r_prev = 0;
+      str->theUserStruct->integ_err_l_prev = 0;
+      str->theUserStruct->integ_err_r_prev = 0;
       
       while((err_angle_gauche>0.02)||(err_angle_gauche<-0.02)||(err_angle_droite<-0.02)||(err_angle_droite>0.02))
       {
-            run_angle_controller(struct, err_angle_gauche, err_angle_droite);
-            err_angle_gauche = struct->theUserStruct->err_langle;
-            err_angle_droite = struct->theUserStruct->err_rangle;
+            run_angle_controller(str, err_angle_gauche, err_angle_droite);
+            err_angle_gauche = str->theUserStruct->err_langle;
+            err_angle_droite = str->theUserStruct->err_rangle;
       }
       return;
 }
