6b3ae0369ef63f90fd032dd2d3cba9618fc3f63d
PierreYvesRaucq <58430139+PierreYvesRaucq@users.noreply.github.com>
2019-12-11 15:36:17 +0100

    Update Rotate.c

--- Rotate.c
+++ Rotate.c
@@ -6,11 +6,14 @@ void rotation(double theta, CtrlStruct* struct)
       double r = 0.03;
       double d = 0.215;
       
-      double err_angle_gauche = -14*(d/(2*r))*theta;
-      double err_angle_droite = 14*(d/(2*r))*theta;
+      double err_angle_gauche = -(d/(2*r))*theta;
+      double err_angle_droite = (d/(2*r))*theta;
       
       struct->theUserStruct->err_langle = err_angle_gauche;
       struct->theUserStruct->err_rangle = err_angle_droite;
+      
+      struct->theUserStruct->integ_err_l_prev = 0;
+      struct->theUserStruct->integ_err_r_prev = 0;
       
       while((err_angle_gauche>0.02)||(err_angle_gauche<-0.02)||(err_angle_droite<-0.02)||(err_angle_droite>0.02))
       {
